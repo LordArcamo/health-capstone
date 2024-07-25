c72dcbd002bcf7617ad780ce0879413fb081d03f
Lord <lordarcamo38@gmail.com>
2024-07-25 23:52:15 +0800

    Dashboard UI Updated

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,6 +13,9 @@ import { Head } from '@inertiajs/vue3';
           gap: 50px;
      }
      .patient-card{
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
           padding: 50px 50px 50px 50px;
           background-color: black;
           border-radius: 20px;
@@ -20,18 +23,26 @@ import { Head } from '@inertiajs/vue3';
           width: 50%;
      }
      .date-card{
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
           padding: 50px 50px 50px 50px;
-          background-color: rgb(47, 0, 134);
+          background-color: rgb(85, 57, 222);
           border-radius: 20px;
           color: white;
           width: 50%;
      }
      .cases-card{
           padding: 50px 50px 50px 50px;
-          background-color: rgb(13, 203, 255);
+          background-color: rgb(194, 231, 241);
           border-radius: 20px;
           color: white;
           width: 100%;
+     }
+
+     p{
+          display: flex;
+          gap: 10px;
      }
 </style>
 
@@ -42,16 +53,19 @@ import { Head } from '@inertiajs/vue3';
         <div>
                <div class="container">
                     <div class="patient-card">
-                         Total Patients
+                         <h1>Total Patients</h1>
+                         <p><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 448 512"><path fill="#ffffff" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg>100,000</p>
                     </div>
                     <div class="date-card">
-                         Date
+                         <h1>Date</h1>
+                         <p><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 448 512"><path fill="#ffffff" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg>100,000</p>
                     </div>
                </div>
                <div class="container">
                     <div class="cases-card">
-                         Cases
-               </div>
+                         <h1>Cases</h1>
+                    </div>
+                    
                </div>
         </div>
    </NewLayout>
