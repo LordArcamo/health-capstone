da8ec47f5f4e54f72c6d7327ae4a6e72f76533d4
Lord <lordarcamo38@gmail.com>
2024-10-13 20:35:30 +0800

    Checkup Edited

--- resources/js/Pages/Checkup.vue
+++ resources/js/Pages/Checkup.vue
@@ -4,17 +4,6 @@ import { Head } from '@inertiajs/vue3';
 import Modal from '../Components/WelcomeModal.vue';
 // import FormComponent from '@/Components/CheckupForm.vue';
 
-
-function submitForm(form) {
-  console.log('Submitting from parent:', form); // Debugging
-  Inertia.post('/checkup/store', form)
-    .then(response => {
-      console.log('Response:', response); // Check the response
-    })
-    .catch(error => {
-      console.error('Error during submission:', error); // Log any errors
-    });
-}
 </script>
 
 <template>
