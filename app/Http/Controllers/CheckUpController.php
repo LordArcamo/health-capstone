3b1f20dd74960113144198eb20c7ed8fff3ee19d
luigie185 <luigie.posod@gmail.com>
2025-01-12 20:49:30 +0800

    ITR PRENATAL STATUS

--- app/Http/Controllers/CheckUpController.php
+++ app/Http/Controllers/CheckUpController.php
@@ -141,7 +141,7 @@ class CheckUpController extends Controller
                 'consultation_details.providerName',
                 'consultation_details.natureOfVisit',
                 'consultation_details.visitType',
-                'consultation_details.status',
+                'consultation_details.status', //kani
                 'visit_information.chiefComplaints',
                 'visit_information.diagnosis',
                 'visit_information.medication'
